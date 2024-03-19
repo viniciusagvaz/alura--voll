78402a69c3f04289e0bce96e2ed210734a0e76ef
viniciusagvaz <agvazvinicius@gmail.com>
2024-03-19 13:37:49 -0300

    Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css';
+import { Avaliacao } from './components/Avaliacao';
+import { Botao } from './components/Botao';
 import { Cabecalho } from './components/Cabecalho';
 import { Container } from './components/Container';
 import { Grafico } from './components/Grafico';
 import { Rodape } from './components/Rodape';
+import { Subtitulo } from './components/Subtitulo';
 import { Tabela } from './components/Tabela';
 import { Titulo } from './components/Titulo';
 import { useDadosConsulta } from './useDadosConsulta';
@@ -19,13 +22,19 @@ export function App() {
 
 	return (
 		<>
-			<Cabecalho />
-			<Container>
-				<Titulo children={`Área administrativa`}></Titulo>
-				<Tabela consultas={consultas}></Tabela>
-				<Grafico consultas={consultas} profissionais={profissionais}/>
-			</Container>
-			<Rodape></Rodape>
+<Cabecalho />
+      <Container>
+        <Titulo>Área Administrativa</Titulo>
+        <Botao>Cadastrar especialista</Botao>
+        <Titulo imagem="consulta">Consultas do Dia</Titulo>
+        <Tabela consultas={consultas} />
+        <Titulo imagem="grafico">Consultas mensais por especialista</Titulo>
+        <Subtitulo>Dezembro/22</Subtitulo>
+        <Grafico consultas={consultas} profissionais={profissionais} />
+        <Titulo imagem="avaliacao">Avaliações de especialistas</Titulo>
+        <Avaliacao profissionais={profissionais} />
+      </Container>
+      <Rodape />
 		</>
 	);
 }
---
+++ src/components/Subtitulo/index.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Subtitulo = styled.h3`
+	color: var(--azul-escuro);
+	font-weight: 700;
+	font-size: 18px;
+`;
